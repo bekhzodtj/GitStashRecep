7136d6ee3165e6fa802af0ecc99589b2656756a0
bekhzodtj <bekhzodtj@gmail.com>
2020-09-24 20:21:26 -0400

    Second commit

--- src/com/Syntax/class02/TestStash.java
+++ src/com/Syntax/class02/TestStash.java
@@ -2,4 +2,8 @@ package com.Syntax.class02;
 
 public class TestStash {
 
+	public static void main(String[] args) {
+		System.out.println("Modification of our code");
+		System.out.println("Hello World");
+	}
 }
