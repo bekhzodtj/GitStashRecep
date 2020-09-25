a2016840bf2fad4631506516a4252daf6e15204e
bekhzodtj <bekhzodtj@gmail.com>
2020-09-24 21:29:11 -0400

    FeatureA comleted

--- src/com/Syntax/class01/CarCalss.java
+++ src/com/Syntax/class01/CarCalss.java
@@ -1,5 +1,12 @@
 package com.Syntax.class01;
 
 public class CarCalss {
+<<<<<<< Updated upstream
 
+=======
+public static void main(String[] args) {
+	System.out.println("I have gray toyota camry");
+	System.out.println("I love sports cars");
+}
+>>>>>>> Stashed changes
 }
---
+++ src/com/Syntax/class02/FeatureB.java
@@ -0,0 +1,10 @@
+package com.Syntax.class02;
+
+public class FeatureB {
+	public static void main(String[] args) {
+		System.out.println("featureB");
+		
+		System.out.println("FeatureA completed");
+	}
+
+}
